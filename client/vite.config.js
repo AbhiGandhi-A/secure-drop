a8dd75c24e9a14ac2d23e6b7b70687a148511727
AbhiGandhi-A <abhigandhi918@gmail.com>
2025-10-12 18:20:27 +0530

    Frontend build ready for Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ important: base must be "/"
 export default defineConfig({
   plugins: [react()],
+  base: "/",  // this fixes broken asset URLs on Vercel
   build: {
-    outDir: 'dist', // required by Vercel
-  },
+    outDir: "dist"
+  }
 })
